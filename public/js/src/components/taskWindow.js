039b78e3b7abe9cfe456b93225df8145618dde13
Lindelor <Leontev150@mail.ru>
2020-10-28 04:15:21 +0400

    taskTab is done!

--- public/js/src/components/taskWindow.js
+++ public/js/src/components/taskWindow.js
@@ -14,41 +14,57 @@ export default function showTask(task, currentEmployee) {
 }
 
 function limitedShowTask(task) {
-	return (
-		webix.ui({
-		view:"window",
-		id:"changeWindow",
-		head:"Задача " + task.name,
-		modal:true,
-		position:"center",
-		width:1000,
-		body:{
-			id:"changeForm",
-			view:"form", 
-			elements:[
-				{ view:"text", label:"Название", name:"taskName", value:task.name, readonly:true, labelWidth:120},
-				{ view:"textarea", label:"Описание", name:"taskDescriprion", value:task.description, readonly:true, height: 150, labelWidth:120},
-				{ view:"text", label:"ID проекта", name:"taskProjectName", value:task.projectName, readonly:true, labelWidth:120},
-				{ view:"select", label:"Статус", options:[TASK_STATUS.haveEmployee, TASK_STATUS.inProgress, TASK_STATUS.done], name:"taskStatus", labelWidth:120, value:task.status},
-				{ view:"select", label:"Срочность", options:[URGENCY.ASAP, URGENCY.Low, URGENCY.NVM], name:"taskUrgency", labelWidth:120, disabled:true, value: task.urgency},
-				{ view:"text", label:"Ожид. Время", name:"taskEstimated", value:task.estimated, labelWidth:120},
-				{ view:"text", label:"Факт. Время", name:"taskFact", value:task.end, labelWidth:120},
-				{ margin:5, cols:[
-					{ view:"button", value:"Изменить" , css:"webix_primary", click:saveChange},
-					{ view:"button", value:"Отменить" , css:"webix_primary", click:discard},
-				]}
-			],
+	employeeModel.getEmployeesIdFIO().then((employees) => {
+		taskModel.getTaskStatuses().then((statuses) => {
+			taskModel.getTaskUrgencies().then((urgencies) => {
+				projectModel.getProjectsNames().then((names) => {
+					return (
+						webix.ui({
+						view:"window",
+						id:"changeWindow",
+						head:"Задача " + task.name,
+						modal:true,
+						position:"center",
+						width:1000,
+						body:{
+							id:"changeForm",
+							view:"form", 
+							elements:[
+								{ view:"text", label:"ID", name:"taskId", value:task.id, disabled:true, labelWidth:120},
+								{ view:"textarea", label:"Описание", name:"taskDescription", value:task.description, height: 120, labelWidth:120, disabled:true},
+								{ view:"select", label:"Проект", options:names, name:"taskProjectName", value:task.projectName, labelWidth:120, disabled:true},
+								{ view:"select", label:"Сотрудник", options:employees, name:"employee", value:task.employee, labelWidth:120, disabled:true},
+								{ view:"select", label:"Статус", options:statuses, name:"taskStatus", labelWidth:120, value:task.status, disabled:true},
+								{ view:"select", label:"Срочность", options:urgencies, name:"taskUrgency", labelWidth:120, value: task.urgency, disabled:true},
+								{ view:"text", label:"Ожид. Время", name:"taskEstimated", value:task.estimated, labelWidth:120},
+								{ view:"text", label:"Факт. Время", name:"taskFact", value:task.end, labelWidth:120},
+								{ margin:5, cols:[
+									{ view:"button", value:"Изменить" , css:"webix_primary", click:saveChange},
+									{ view:"button", value:"Завершить" , css:"webix_primary", click:closeTask},
+									{ view:"button", value:"На согласование" , css:"webix_primary", click:toReconcilation},
+									{ view:"button", value:"Отменить" , css:"webix_primary", click:discard},
+								]}
+							],
+							rules:{
+								"taskDescription":webix.rules.isNotEmpty,
+								"taskEstimated":webix.rules.isNumber,
+								"taskFact":webix.rules.isNumber,
+							  },
+							  elementsConfig:{
+								on:{
+								  "onChange":function(){
+									this.validate();
+								  }
+								}
+							  }
+						}
+						}).show()
+					)
+				})
 
-			rules:{
-				"taskName":webix.rules.isNotEmpty,
-				"taskDescription":webix.rules.isNotEmpty,
-				"taskProjectName":webix.rules.isNumber,
-				"taskEstimated":webix.rules.isNumber,
-				"taskFact":webix.rules.isNumber,
-			  }
-		}
-		}).show()
-	)
+			})
+		})
+	})
 	
 }
 
